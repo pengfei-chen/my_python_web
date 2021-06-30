e246a5404644f3856476d181950096e4dea7671f
pengfei-chen <932564973@qq.com>
2021-06-30 06:28:56 -0700

    0630

--- www/Myblog/blog/admin.py
+++ www/Myblog/blog/admin.py
@@ -5,7 +5,19 @@ from .models import Category, Tag, Post
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'status', 'owner', 'is_nav', 'post_count','created_time')
+    fields = ('name', 'status',  'is_nav')
+
+    # select * from Post p where p.category = 'Demo'
+    def post_count(self, obj):
+        return f"{obj.post_set.count()}篇文章"
+        
+    post_count.short_description = "文章数"
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        print(request.user)
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -13,5 +25,17 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('category','title', 'owner', 'status')
+    exclude =('owner',)
+
+    actions_on_top = True # 首页的按钮
+    actions_on_bottom = True # 首页的按钮
+
+    save_on_top = True # 编辑页面的按钮
+    search_fields = ('title','category__name')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        print(request.user)
+        return super(PostAdmin, self).save_model(request, obj, form, change)
     
